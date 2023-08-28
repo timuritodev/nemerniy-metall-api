583b3be87c590d86a10309e356e429518fadaf93
luciD <sosalnet@yandex.ru>
2023-08-28 04:53:52 +0300

    fixed repo files

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-# movies-explorer-api
-Репозиторий для приложения проекта `Movies-mesto`, включающий бэкенд приложения со следующими возможностями.
-* [Ссылка на репозиторий](https://github.com/sosalnet/movies-explorer-api)
-* IP - 51.250.84.39
-* Backend - api.diploma-sosalnet.nomoredomains.club
+# nemernyi-metall-api
--- controllers/cards.js
+++ controllers/cards.js
@@ -3,21 +3,21 @@ const { Card } = require('../models/card');
 const NotFoundError = require('../errors/NotFoundError');
 
 module.exports.getCards = (req, res, next) => {
-    Card.find()
-        .then((cards) => {
-            res.send(cards);
-        })
-        .catch((err) => next(new ServerError(err.message)));
+  Card.find()
+    .then((cards) => {
+      res.send(cards);
+    })
+    .catch((err) => next(new ServerError(err.message)));
 };
 
 module.exports.getCardById = (req, res, next) => {
-    const cardId = req.params.id;
-    Card.findOne({ id: cardId })
-        .then((card) => {
-            if (!card) {
-                throw new NotFoundError('Карточка не найдена');
-            }
-            res.send(card);
-        })
-        .catch((err) => next(new ServerError(err.message)));
+  const cardId = req.params.id;
+  Card.findOne({ id: cardId })
+    .then((card) => {
+      if (!card) {
+        throw new NotFoundError('Карточка не найдена');
+      }
+      res.send(card);
+    })
+    .catch((err) => next(new ServerError(err.message)));
 };
